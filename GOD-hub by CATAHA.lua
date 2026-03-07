562506adc91a8cbf1b4fa63dfbe80feeb659c60a
az3422 <igorkozhin0203@gmail.com>
2026-03-07 16:26:49 +0300

    Update GOD-hub by CATAHA.lua

--- GOD-hub by CATAHA.lua
+++ GOD-hub by CATAHA.lua
@@ -1,4 +1,4 @@
-ocal Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 local Window = Library.CreateLib("CATAHA hub", "RJTheme3")
 
