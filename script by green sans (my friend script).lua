697401cf5026ea67bf06b9471ece8dc6e6723f4e
az3422 <igorkozhin0203@gmail.com>
2026-03-15 10:13:38 +0300

    Update script by green sans (my friend script).lua

--- script by green sans (my friend script).lua
+++ script by green sans (my friend script).lua
@@ -21,6 +21,7 @@ local RiftHack = false
 local InformationGuiEnable = false
 local SureDeleteThis = false
 local CustomOpened = false
+local GodModesOpened = false
 
 local sc = Instance.new("ScreenGui")
 sc.Parent = game.Players.LocalPlayer.PlayerGui
@@ -30,7 +31,7 @@ local bb = Instance.new("TextButton")
 bb.Size = UDim2.new(0.1, 0, 0.05, 0)
 bb.Position = UDim2.new(0.45, 0, 0.86, 0)
 bb.TextScaled = true
-bb.Text = "open portal menu"
+bb.Text = "Открыть меню телепорта"
 bb.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb.Name = "TB1"
 
@@ -42,7 +43,7 @@ local bb2 = Instance.new("TextButton")
 bb2.Size = UDim2.new(0.05, 0, 0.025, 0)
 bb2.Position = UDim2.new(0.01, 0, 0.97, 0)
 bb2.TextScaled = true
-bb2.Text = "hide buttons"
+bb2.Text = "Скрыть кнопки"
 bb2.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb2.Name = "TB2"
 
@@ -62,7 +63,7 @@ local bb3 = Instance.new("TextButton")
 bb3.Size = UDim2.new(0.05, 0, 0.025, 0)
 bb3.Position = UDim2.new(0.01, 0, 0.97, 0)
 bb3.TextScaled = true
-bb3.Text = "show buttons"
+bb3.Text = "Показать кнопки"
 bb3.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb3.Visible = false
 bb3.Name = "TB3"
@@ -83,7 +84,7 @@ local bb4 = Instance.new("TextButton")
 bb4.Size = UDim2.new(0.09, 0, 0.04, 0)
 bb4.Position = UDim2.new(0.01, 0, 0.86, 0)
 bb4.TextScaled = true
-bb4.Text = "activate main functions"
+bb4.Text = "Актирировать основные функции"
 bb4.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb4.Name = "TB4"
 
@@ -109,7 +110,7 @@ local bb5 = Instance.new("TextButton")
 bb5.Size = UDim2.new(0.09, 0, 0.04, 0)
 bb5.Position = UDim2.new(0.01, 0, 0.86, 0)
 bb5.TextScaled = true
-bb5.Text = "create part that will help get to the grillby's roof"
+bb5.Text = "Создать объект который помогает залезть на крышу гриллби"
 bb5.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb5.Name = "TBDeleted1"
 bb5.Visible = false
@@ -144,7 +145,7 @@ local bb8 = Instance.new("TextButton")
 bb8.Size = UDim2.new(0.09, 0, 0.04, 0)
 bb8.Position = UDim2.new(0.11, 0, 0.91, 0)
 bb8.TextScaled = true
-bb8.Text = "activateCustomEvade without Cooldown (Q for customevade)"
+bb8.Text = "Врубить CustomEvade без Cooldown (Используйте кнопку Q для customevade)"
 bb8.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb8.Name = "TB8"
 
@@ -171,7 +172,7 @@ local bb9 = Instance.new("TextButton")
 bb9.Size = UDim2.new(0.09, 0, 0.04, 0)
 bb9.Position = UDim2.new(0.21, 0, 0.86, 0)
 bb9.TextScaled = true
-bb9.Text = "hide trees"
+bb9.Text = "Убрать деревья"
 bb9.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb9.Name = "TB9"
 
@@ -188,10 +189,10 @@ bb9.MouseButton1Click:Connect(function()
 
 	if TreesDeleted then
 		test2()
-		bb9.Text = "hide trees"
+		bb9.Text = "Убрать деревья"
 	else
 		test()
-		bb9.Text = "return trees"
+		bb9.Text = "Вернуть деревья"
 	end
 end)
 
@@ -199,7 +200,7 @@ local bb10 = Instance.new("TextButton")
 bb10.Size = UDim2.new(0.09, 0, 0.04, 0)
 bb10.Position = UDim2.new(0.01, 0, 0.91, 0)
 bb10.TextScaled = true
-bb10.Text = "dupe tool in hands [client]"
+bb10.Text = "Дюпнуть предмет в руках [Клиент]"
 bb10.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb10.Name = "TB10"
 
@@ -216,7 +217,7 @@ T1.TextScaled = true
 T1.BackgroundTransparency = 0.42
 T1.TextEditable = false
 T1.ClearTextOnFocus = false
-T1.Text = "open portal menu: opens GUI portal's from USB"
+T1.Text = "Открыть меню телепорта: открывает GUI телепорта из USB"
 T1.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 T1.Name = "Text1"
 T1.Visible = false
@@ -228,7 +229,7 @@ T2.TextScaled = true
 T2.BackgroundTransparency = 0.42
 T2.TextEditable = false
 T2.ClearTextOnFocus = false
-T2.Text = "acivate main functions: delete cd for portals + creates part that helps get to the grillby's roof"
+T2.Text = "Актирировать основные функции: вырубает задержку у телепортов во всех локациях + создает объект который помогает залезть на крышу гриллби"
 T2.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 T2.Name = "Text2"
 T2.Visible = false
@@ -240,7 +241,7 @@ T3.TextScaled = true
 T3.BackgroundTransparency = 0.42
 T3.TextEditable = false
 T3.ClearTextOnFocus = false
-T3.Text = "Hack-Rift: teleports Rift от fatal_error_sans to you, also deletes cd for it"
+T3.Text = "Hack-Rift: телепортирует Rift от fatal_error_sans к вам, также вырубает у него задержку"
 T3.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 T3.Name = "Text3"
 T3.Visible = false
@@ -252,7 +253,7 @@ T4.TextScaled = true
 T4.BackgroundTransparency = 0.42
 T4.TextEditable = false
 T4.ClearTextOnFocus = false
-T4.Text = "activate CustomEvade: activates CustomEvade with short key Q, CustomEvade is turning off after death."
+T4.Text = "Врубить CustomEvade: врубает CustomEvade с биндом на Q, если-что CustomEvade выключается после смерти/ресета не забывайте об этом!"
 T4.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 T4.Name = "Text4"
 T4.Visible = false
@@ -264,7 +265,7 @@ T5.TextScaled = true
 T5.BackgroundTransparency = 0.42
 T5.TextEditable = false
 T5.ClearTextOnFocus = false
-T5.Text = "hide trees:deletes all trees in snowdin (client) can return"
+T5.Text = "Убрать деревья: убирает все деревья в Snowdin, можно вернуть"
 T5.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 T5.Name = "Text5"
 T5.Visible = false
@@ -276,7 +277,7 @@ T6.TextScaled = true
 T6.BackgroundTransparency = 0.42
 T6.TextEditable = false
 T6.ClearTextOnFocus = false
-T6.Text = "dupe tool in hands: dupes tool in your hands (client) down work on all tools in game"
+T6.Text = "Дюпнуть предмет в руках: при нажатии у вас дюпнится предмет в инвентарь который вы держите, однако работает на очень маленькое количество предметов!"
 T6.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 T6.Name = "Text6"
 T6.Visible = false
@@ -288,7 +289,7 @@ T7.TextScaled = true
 T7.BackgroundTransparency = 0.42
 T7.TextEditable = false
 T7.ClearTextOnFocus = false
-T7.Text = "random tp: teleports to Rift в Anti-void (if doesnt work, use hack-rift and teleport to the anti-void, after this must work)"
+T7.Text = "Рандомная телепортация: телепортирует в Rift в Anti-void (если кнопка не работает используйте hack-rift и тп в anti-void, после этого она должна работать)"
 T7.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 T7.Name = "Text7"
 T7.Visible = false
@@ -297,7 +298,7 @@ local bb11 = Instance.new("TextButton")
 bb11.Size = UDim2.new(0.05, 0, 0.025, 0)
 bb11.Position = UDim2.new(0.07, 0, 0.97, 0)
 bb11.TextScaled = true
-bb11.Text = "shop tip"
+bb11.Text = "Показать подсказку"
 bb11.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb11.Name = "TB11"
 
@@ -310,7 +311,7 @@ bb11.MouseButton1Click:Connect(function()
 		game.Players.LocalPlayer.PlayerGui.ScreenGui.Text5.Visible = false
 		game.Players.LocalPlayer.PlayerGui.ScreenGui.Text6.Visible = false
 		game.Players.LocalPlayer.PlayerGui.ScreenGui.Text7.Visible = false
-		bb11.Text = "show tip"
+		bb11.Text = "Показать подсказку"
 		InformationGuiEnable = false
 	else
 		game.Players.LocalPlayer.PlayerGui.ScreenGui.Text1.Visible = true
@@ -320,7 +321,7 @@ bb11.MouseButton1Click:Connect(function()
 		game.Players.LocalPlayer.PlayerGui.ScreenGui.Text5.Visible = true
 		game.Players.LocalPlayer.PlayerGui.ScreenGui.Text6.Visible = true
 		game.Players.LocalPlayer.PlayerGui.ScreenGui.Text7.Visible = true
-		bb11.Text = "hide tip"
+		bb11.Text = "Скрыть подсказку"
 		InformationGuiEnable = true
 	end		
 end)
@@ -329,7 +330,7 @@ local bb12 = Instance.new("TextButton")
 bb12.Size = UDim2.new(0.05, 0, 0.025, 0)
 bb12.Position = UDim2.new(0.13, 0, 0.97, 0)
 bb12.TextScaled = true
-bb12.Text = "close + destroy ScreenGui"
+bb12.Text = "Уничтожить ScreenGui"
 bb12.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb12.Name = "TB12"
 bb12.BackgroundColor3 = Color3.fromRGB(165, 0, 0)
@@ -338,7 +339,7 @@ bb12.MouseButton1Click:Connect(function()
 	if SureDeleteThis then
 		game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
 	else
-		bb12.Text = "click once more to delete GUI"
+		bb12.Text = "Нажмите ещё раз для удаления GUI"
 		SureDeleteThis = true
 	end		
 end)
@@ -347,7 +348,7 @@ local bb13 = Instance.new("TextButton")
 bb13.Size = UDim2.new(0.09, 0, 0.04, 0)
 bb13.Position = UDim2.new(0.21, 0, 0.91, 0)
 bb13.TextScaled = true
-bb13.Text = "random tp"
+bb13.Text = "Рандомная телепортация"
 bb13.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb13.Name = "TB13"
 
@@ -359,10 +360,10 @@ local bb14 = Instance.new("TextButton")
 bb14.Size = UDim2.new(0.05, 0, 0.025, 0)
 bb14.Position = UDim2.new(0.19, 0, 0.97, 0)
 bb14.TextScaled = true
-bb14.Text = "customisation"
+bb14.Text = "Кастомизация"
 bb14.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb14.Name = "TB14"
-bb14.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+bb14.BackgroundColor3 = Color3.fromRGB(0, 255, 255)
 
 bb14.MouseButton1Click:Connect(function()
 	if CustomOpened then
@@ -383,7 +384,7 @@ TS1.TextScaled = true
 TS1.BackgroundTransparency = 0
 TS1.TextEditable = true
 TS1.ClearTextOnFocus = false
-TS1.Text = "write your R(Red), G(Green), B(Blue) here"
+TS1.Text = "напиши свой R(Red), G(Green), B(Blue) суда"
 TS1.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 TS1.Name = "SpecialText1"
 TS1.Visible = false
@@ -392,7 +393,7 @@ local bb15 = Instance.new("TextButton")
 bb15.Size = UDim2.new(0.1, 0, 0.05, 0)
 bb15.Position = UDim2.new(0.03, 0, 0.4125, 0)
 bb15.TextScaled = true
-bb15.Text = "color all buttons into this color"
+bb15.Text = "Перекрасить все кнопки в ваш цвет"
 bb15.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
 bb15.Name = "TB15"
 bb15.BackgroundColor3 = Color3.fromRGB(150, 150, 150)
@@ -419,10 +420,106 @@ bb15.MouseButton1Click:Connect(function()
 			game.Players.LocalPlayer.PlayerGui.ScreenGui.TB9.BackgroundColor3 = Color3.fromRGB(r, g, b)
 			game.Players.LocalPlayer.PlayerGui.ScreenGui.TB10.BackgroundColor3 = Color3.fromRGB(r, g, b)
 			game.Players.LocalPlayer.PlayerGui.ScreenGui.TB13.BackgroundColor3 = Color3.fromRGB(r, g, b)
+			game.Players.LocalPlayer.PlayerGui.ScreenGui.TB2.BackgroundColor3 = Color3.fromRGB(r, g, b)
+			game.Players.LocalPlayer.PlayerGui.ScreenGui.TB3.BackgroundColor3 = Color3.fromRGB(r, g, b)
+			game.Players.LocalPlayer.PlayerGui.ScreenGui.TB15.BackgroundColor3 = Color3.fromRGB(r, g, b)
+			game.Players.LocalPlayer.PlayerGui.ScreenGui.TB11.BackgroundColor3 = Color3.fromRGB(r, g, b)
         else
-            warn("must be 0-255,")
+            warn("Значения должны быть от 0-255, не больше")
         end
     else
-        warn("wrong one, correct one: 0, 255, 0")
+        warn("Ты ввёл не правильное значение, пример правильного значения: 0, 255, 0")
     end
+end)
+
+local bb16 = Instance.new("TextButton")
+bb16.Size = UDim2.new(0.05, 0, 0.025, 0)
+bb16.Position = UDim2.new(0.25, 0, 0.97, 0)
+bb16.TextScaled = true
+bb16.Text = "Иммунитеты [ALPHA]"
+bb16.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
+bb16.Name = "TB16"
+bb16.BackgroundColor3 = Color3.fromRGB(255, 0, 255)
+
+bb16.MouseButton1Click:Connect(function()
+	if GodModesOpened then
+		game.Players.LocalPlayer.PlayerGui.ScreenGui.IB1.Visible = false
+		game.Players.LocalPlayer.PlayerGui.ScreenGui.IB2.Visible = false
+		game.Players.LocalPlayer.PlayerGui.ScreenGui.IB3.Visible = false
+		GodModesOpened = false
+	else
+		game.Players.LocalPlayer.PlayerGui.ScreenGui.IB1.Visible = true
+		game.Players.LocalPlayer.PlayerGui.ScreenGui.IB2.Visible = true
+		game.Players.LocalPlayer.PlayerGui.ScreenGui.IB3.Visible = true
+		GodModesOpened = true
+	end
+end)
+
+local function IB1Function()
+	while true do
+		wait(0.1)
+		if game.Workspace:FindFirstChild("km_gameover1") then
+			game.Workspace.km_gameover1:destroy()
+		end
+	end
+end
+
+local IB1 = Instance.new("TextButton")
+IB1.Size = UDim2.new(0.05, 0, 0.025, 0)
+IB1.Position = UDim2.new(0.01, 0, 0.58, 0)
+IB1.TextScaled = true
+IB1.Text = "Anti-GameOver"
+IB1.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
+IB1.Name = "IB1"
+IB1.BackgroundColor3 = Color3.fromRGB(0, 120, 0)
+IB1.Visible = false
+
+IB1.MouseButton1Click:Connect(function()
+	IB1Function()
+end)
+
+local function IB2Function()
+	while true do
+		wait(0.1)
+		if game.Workspace:FindFirstChild("km_chainzone") then
+			game.Workspace.km_chainzone:destroy()
+		end
+	end
+end
+
+local IB2 = Instance.new("TextButton")
+IB2.Size = UDim2.new(0.05, 0, 0.025, 0)
+IB2.Position = UDim2.new(0.07, 0, 0.58, 0)
+IB2.TextScaled = true
+IB2.Text = "Anti-KmChainZone"
+IB2.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
+IB2.Name = "IB2"
+IB2.BackgroundColor3 = Color3.fromRGB(0, 120, 0)
+IB2.Visible = false
+
+IB2.MouseButton1Click:Connect(function()
+	IB2Function()
+end)
+
+local function IB3Function()
+	while true do
+		wait(0.1)
+		if game.Workspace:FindFirstChild("m87_bz") then
+			game.Workspace.m87_bz:destroy()
+		end
+	end
+end
+
+local IB3 = Instance.new("TextButton")
+IB3.Size = UDim2.new(0.05, 0, 0.025, 0)
+IB3.Position = UDim2.new(0.13, 0, 0.58, 0)
+IB3.TextScaled = true
+IB3.Text = "Anti-M87_bz"
+IB3.Parent = game.Players.LocalPlayer.PlayerGui.ScreenGui
+IB3.Name = "IB3"
+IB3.BackgroundColor3 = Color3.fromRGB(0, 120, 0)
+IB3.Visible = false
+
+IB3.MouseButton1Click:Connect(function()
+	IB3Function()
 end)
